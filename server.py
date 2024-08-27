0a3ff8c1ac5684d6e3ada6169506b60c54f68191
DannyIsa <daniisakov@gmail.com>
2024-08-27 13:52:01 +0300

    dba 4

--- server.py
+++ server.py
@@ -1,9 +1,17 @@
 import socket
 import threading
-from quic import handle_server
+
+def send_file(self, filepath):
+        with open(filepath, 'rb') as f:
+            packet_size = random.randint(1000, 2000)
+            while True:
+                data = f.read(packet_size)
+                if not data:
+                    break
+                self.connection.sendall(data)
 
 
-def start_server(host, port, file_to_send):
+def start_server(host, port, files_to_send):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, port))
     server_socket.listen(10) # 10 users maximum
@@ -12,9 +20,23 @@ def start_server(host, port, file_to_send):
 
 
     while True:
-        sock, addr = server_socket.accept()
+        client_sock, addr = server_socket.accept()
         print(f"Connection from {addr}")
-        threading.Thread(target=handle_server,args=(sock, file_to_send)).start()
+
+        threads = []
+        for file in files_to_send:
+            threads.append(threading.Thread(target=send_file,args=(client_sock, file)))
+        
+        # Start all threads
+        for thread in threads:
+            thread.start()
+
+        # Wait for all threads to finish
+        for thread in threads:
+            thread.join()
+        
+        client_sock.close()
+
 
 
 if __name__ == "__main__":
