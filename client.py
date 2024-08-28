a3f2888c01c23aaf8cabec08115c41c99222f2cf
Daniel Isakov <daniisakov@gmail.com>
2024-08-28 14:47:17 +0300

    hashmap 1

--- client.py
+++ client.py
@@ -1,45 +1,60 @@
 import socket
 
-BUFFER_SIZE = 4096 
-HEADER_SIZE = 256 # header is a fixed 256 bytes, accommodating the file name and file size
+BUFFER_SIZE = 1024
+HEADER_SIZE = 256  # header is a fixed 256 bytes, accommodating the file name and file size
+
 
 def request_all_files(connection):
     # Client continuously requests the next file in "queue", if such exists
     while True:
-        if not request_file(connection):
-            print("All files receieved. Closing connection.")
-            break
+        files_dict = {}
 
-def request_file(connection):
-    # First receive the header length and format it
-    header = connection.recv(HEADER_SIZE).decode('utf-8').strip()
-    
-    # Check if there is no more data coming from the server
-    if not header:
-        return False  # No more files to receive
-    
-    file_name, file_size_str = header.split(':')
-    file_size = int(file_size_str)
-     
+        file_received = request_file(connection, files_dict)
+        while file_received:
+            file_received = request_file(connection, files_dict)
+
+        for file_name in files_dict:
+            with open("new_" + file_name, 'wb') as f:
+                for data_id in sorted(files_dict[file_name]):
+                    f.write(files_dict[file_name][data_id])
+
+
+def request_file(connection, files_dict):
+    frame = connection.recv(BUFFER_SIZE)
+
+    if not frame:
+        return False
+
+    file_name, data_id, data = frame.split(':')
+    print(f"Received file: {file_name}, data_id: {data_id}s")
+    data_id = int(data_id)
+
+    if file_name not in files_dict:
+        files_dict[file_name] = {data_id: data}
+    else:
+        files_dict[file_name][data_id] = data
+
     # ! Notice a file sent as "file1.txt" is saved as "new_file1.txt" just so files won't override eachother since they are in the same directory.
-    with open("new_" + file_name, 'wb') as f:
-        bytes_received = 0
-        while bytes_received < file_size:
-            remaining = file_size - bytes_received
-            frame = connection.recv(min(BUFFER_SIZE, remaining))
-            if not frame:
-                break
-            f.write(frame)
-            bytes_received += len(frame)
-    
-    print(f"Successfully received file: {file_name}, size: {file_size} bytes")
+    # with open("new_" + file_name, 'wb') as f:
+    #     bytes_received = 0
+    #     while bytes_received < file_size:
+    #         remaining = file_size - bytes_received
+    #         frame = connection.recv(min(BUFFER_SIZE, remaining))
+    #         if not frame:
+    #             break
+    #         f.write(frame)
+    #         bytes_received += len(frame)
+
+    # print(f"Successfully received file: {file_name}, size: {file_size} bytes")
     return True
+
 
 def connect_to_server(host, port):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((host, port))
     print(f"Connected to {host}:{port}")
     return client_socket
+
 
 # For simplicity's sake, arguments are provided here
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -3,34 +3,29 @@ import threading
 import random
 import os
 
-BUFFER_SIZE = 4096 
+BUFFER_SIZE = 4096
 HEADER_SIZE = 256  # header is a fixed 256 bytes, accommodating the file name and file size
 
-socket_lock = threading.Lock() # Returns a lock object that can be used to synchronize the threads.
 
-def create_header(filepath):
-    file_name = os.path.basename(filepath)
-    file_size = os.path.getsize(filepath)
-    header = f"{file_name}:{file_size}".encode('utf-8')
-    header += b' ' * (256 - len(header))  # Padding to ensure header is exactly 256 bytes
-    return header
+def inject_file_data(filepath, data, id):
+    # Inject the file name and id into the data
+    return f"{filepath}:{id}:{data}"
+
 
 def send_file(filepath, client_socket):
     with open(filepath, 'rb') as f:
-        header = create_header(filepath)
-        print(f"header={header}")
-        
         # Multiple threads cannot simultaneously run this code, allowing for some synchornization.
-        with socket_lock:
-            client_socket.sendall(header)  # At first, send the header 
-            packet_size = random.randint(1000, 2000)
-            while True:
-                data = f.read(packet_size)
-                if not data:
-                    break
-                client_socket.sendall(data)
+        packet_size = random.randint(1000, 1800)
+        counter = 1
+        while True:
+            original_data = f.read(packet_size)
+            injected_data = inject_file_data(filepath, original_data, counter)
+            if not injected_data:
+                break
+            client_socket.sendall(injected_data)
+            counter += 1
     print(f"Finished sending {filepath}")
-    
+
 
 def serve_client(client_socket, files_to_send):
     print(f"Handling client {client_socket.getpeername()}")
@@ -39,8 +34,9 @@ def serve_client(client_socket, files_to_send):
     for i in range(len(files_to_send)):
         file = files_to_send[i]
         # Create a thread (stream) for each file to send
-        streams.append(threading.Thread(target=send_file, args=(file, client_socket)))
-    
+        streams.append(threading.Thread(
+            target=send_file, args=(file, client_socket)))
+
     # Start all threads
     for stream in streams:
         stream.start()
@@ -51,6 +47,7 @@ def serve_client(client_socket, files_to_send):
 
     print(f"All files sent to client {client_socket.getpeername()}")
     client_socket.close()  # Close connection after sending all files
+
 
 def start_server(host, port, files_to_send):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -63,14 +60,16 @@ def start_server(host, port, files_to_send):
         while True:
             client_socket, addr = server_socket.accept()
             print(f"Connection from {addr}")
-            serve_client(client_socket,files_to_send)
+            serve_client(client_socket, files_to_send)
     except KeyboardInterrupt:
         print("Server is shutting down.")
     finally:
         server_socket.close()
 
+
 if __name__ == "__main__":
     host = 'localhost'
     port = 9994
-    files = ["file1.txt", "file2.txt", "file3.txt","file4.txt","file5.txt"]  # Assume these are the files available in the server directory
+    # Assume these are the files available in the server directory
+    files = ["file1.txt", "file2.txt", "file3.txt", "file4.txt", "file5.txt"]
     start_server(host, port, files)
