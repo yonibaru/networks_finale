f500477e0214e140f2697c52767e5bf4867add71
yaru² <yonibaru@users.noreply.github.com>
2024-08-31 14:19:55 +0300

    header rework + pdf progress

--- server.py
+++ server.py
@@ -4,18 +4,43 @@ import random
 import os
 
 MAX_CLIENTS = 10
+FILENAME_LENGTH = 32
+PACKET_ID_LENGTH = 7
+PACKET_SIZE_LENGTH = 4
+HEADER_SIZE = FILENAME_LENGTH + PACKET_ID_LENGTH + PACKET_SIZE_LENGTH
 
 '''
 Creates an appropriate header that would be sent along with the packet data to the client.
+Headers are of the following format:
+file_name:frame_id:packet_size:file_data
+    /\       /\        /\         
+     |        |         |
+32 bytes   7 bytes     4 bytes
+
+TOTAL SIZE: 43 BYTES (Without the :)
+
 '''
 def inject_file_data(filepath, data, id, packet_size):
     # Inject the file path, data id, packet size, and data into the data
     # Conceptually, this is equivilant to creating a header.
     if not data:
         return None
-    injected_data = f"{filepath}:{id}:{packet_size}:{data}"
-    return injected_data
-
+    # .zfill simply adds 0's to the start of a string until it reaches the desired length
+    formatted_filename = filepath[:FILENAME_LENGTH].ljust(FILENAME_LENGTH)
+    formatted_packet_id = str(id).zfill(PACKET_ID_LENGTH)[:PACKET_ID_LENGTH]
+    formatted_packet_size = str(packet_size).zfill(PACKET_SIZE_LENGTH)[:PACKET_SIZE_LENGTH]
+    
+    # Form a 43-byte header
+    formatted_header = (f"{formatted_filename}{formatted_packet_id}{formatted_packet_size}")
+    # print(f"formatted_header={formatted_header} size={len(formatted_header)}")
+    # print(f"formatted_filename={formatted_filename} size={len(formatted_filename)}")
+    # print(f"formatted_packet_id={formatted_packet_id} size={len(formatted_packet_id)}")
+    # print(f"formatted_packet_size={formatted_packet_size} size={len(formatted_packet_size)}")
+    
+    if len(formatted_header) != HEADER_SIZE:
+        raise ValueError(f"Header size is not of the required {HEADER_SIZE} bytes. Current size: {len(formatted_header)}")
+    
+    return f"{formatted_header}{data}"
 
 '''
 This function sends a file to a client socket
@@ -37,14 +62,14 @@ def send_file(filepath, client_socket):
                 packet_size = len(original_data)
 
             # Inject the file data into the original data
-            injected_data = inject_file_data(filepath, original_data, counter, packet_size).encode('utf-8')
-            if not injected_data or len(injected_data) == 0:
+            formatted_data = inject_file_data(filepath, original_data, counter, packet_size).encode('utf-8')
+            if not formatted_data or len(formatted_data) == 0:
                 break
             # print("### SENT FRAME: ###")
             # print(f"{injected_data}")
             # Send the injected data to the client
-            client_socket.sendall(injected_data)
-            total_bytes_sent += len(injected_data)
+            client_socket.sendall(formatted_data)
+            total_bytes_sent += len(formatted_data)
             packet_count += 1
             counter += 1
 
@@ -104,7 +129,7 @@ def start_server(host, port, files_to_send):
 
 if __name__ == "__main__":
     host = 'localhost'
-    port = 3000
+    port = 3006
     # The files that would be sent to each client (if any are missing from the server directory they would be skipped)
     files = ["file1.txt","file2.txt","file3.txt","file4.txt","file5.txt","file6.txt","file7.txt","file8.txt","file9.txt","file10.txt"]
     start_server(host, port, files)
